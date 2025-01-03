69b64f6b5b7921a77e80038db348e3e2f495b34e
Pramodk2006 <pramod3132006@gmail.com>
2025-01-03 12:02:02 +0530

    Update and rename image encription.py to Image Encription and Decription.py

--- Image Encription and Decription.py
+++ Image Encription and Decription.py
@@ -3,7 +3,7 @@ def encript(image):
     image=file.read()
     file.close()
     image=bytearray(image)
-    key=int(input())
+    key=int(input("Enter a key value: "))
     for i,j in enumerate(image):
         image[i]=j^key
     en=open("encripted.jpg","wb")
@@ -14,15 +14,18 @@ def decript():
     image=file.read()
     file.close()
     image=bytearray(image)
-    key=int(input())
+    key=int(input("Enter the key : "))
     for i,j in enumerate(image):
         image[i]=j^key
     en=open("decripted.jpg","wb")
     en.write(image)
     en.close()
-image=input("Enter the filename which is to be encripted:")
-i=input()
+
+i=input("Enter e for encription or d for decription: ")
 if i=="e":
+    image=input("Enter the filename which is to be encripted:")
     encript(image)
+    print("Encripted successfully!!!")
 else:
     decript()
+    print("Decripted successfully!!!")
